2c00014a471b2ceb49b75f2f2ae1c5520f52d912
Ryan Neudorf <305650782+rhneudorf@users.noreply.github.com>
2026-08-09 22:17:41 -0400

    Set up GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/RyanReport",
+  assetPrefix: "/RyanReport/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
